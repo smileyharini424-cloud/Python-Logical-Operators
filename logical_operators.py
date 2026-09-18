c93d0c1a3464f24bfc374d0f5eda8b8454f45a60
smileyharini424-cloud <smileyharini424@gmail.com>
2026-09-18 07:51:02 -0700

    Create logical_operators.py

---
+++ logical_operators.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+condition1 = a > 5
+condition2 = b > 10
+
+print("AND:", condition1 and condition2)
+print("OR:", condition1 or condition2)
+print("NOT:", not condition1)
